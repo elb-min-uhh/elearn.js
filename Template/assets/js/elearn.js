7c201969b14bbb1821f05ce3398b0bc4f228ca38
Arne Westphal <westphal.arne@web.de>
2016-10-07 12:31:32 +0200

    ADD: Gelesen deaktivierbar. Sub, subsub-sections
    Man kann in dem Kachelinhaltsverzeichnis den gelesen Haken deaktivieren.
    Zusätzlich gibt es (für die Listenansicht) Sub- und Subsubsections.
    (class="sub" ..="subsub")
    Man kann sections im Inhaltsverzeichnis ausblenden mit
    class="hide-in-overview"

--- Template/assets/js/elearn.js
+++ Template/assets/js/elearn.js
@@ -555,19 +555,51 @@ var sectionsVisited = [];
 function createContentOverview() {
     if($('#content-overview').length > 0) {
         var text = "<ul>";
+        var level = 0;
+        var levels = {
+            SUB : 1,
+            SUBSUB : 2
+        };
 
         for(var i=0; i<$('section').length; i++) {
-            text += "<li onclick='showSection("+i+")'>";
-            text += "<div class='sectionRead'><div class='img'></div></div>";
-            text += "<span class='title'>" + $($('section')[i]).attr('name') + "</span>";
-            if($($('section')[i]).attr('desc') != undefined
-                && $($('section')[i]).attr('desc').length > 0) {
-                text += "<p>" + $($('section')[i]).attr('desc') + "</p>";
+
+            var sec = $($('section')[i]);
+
+            if(sec.is('.hide-in-overview')) continue;
+
+            var sec_level = 0;
+            if(sec.is('.sub')) sec_level = levels.SUB;
+            if(sec.is('.subsub')) sec_level = levels.SUBSUB;
+
+            // higher level
+            while(level < sec_level) {
+                text += "<ul>";
+                level++;
             }
+            // lower level
+            while(level > sec_level) {
+                text += "</ul>";
+                level--;
+            }
+
+            text += "<li onclick='showSection("+i+")'>";
+
+            text += "<div class='sectionRead'><div class='img'></div></div>";
+            text += "<span class='title'>" + sec.attr('name') + "</span>";
+            if(sec.attr('desc') != undefined
+                && sec.attr('desc').length > 0) {
+                text += "<p>" + sec.attr('desc') + "</p>";
+            }
+
             text += "</li>";
+
             sectionsVisited.push(false);
         }
-        text+="</ul>";
+        // close all open ul's
+        while(level >= 0) {
+            text+="</ul>";
+            level--;
+        }
 
         $('#content-overview').html(text);
         $('#content-overview').find("a").click(function(e) {
@@ -580,7 +612,7 @@ function createContentOverview() {
 function updateContentOverview() {
     sectionsVisited[visSection] = true;
     for(var i=0; i<$('section').length; i++) {
-        if(sectionsVisited[i]) {
+        if(sectionsVisited[i] && !$('#content-overview').is('.hide-read')) {
             $($('#content-overview').find('li').get(i)).find('.sectionRead').addClass('read');
         }
         else {
