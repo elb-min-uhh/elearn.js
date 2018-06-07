a07af0a273c1d982edc5d3ad9b4eebe2d93b4878
Arne Westphal <westphal.arne@web.de>
2018-06-07 12:36:02 +0200

    Update Version to v1.0.4

--- Template/assets/js/elearn.js
+++ Template/assets/js/elearn.js
@@ -1,5 +1,5 @@
 /*
-* v1.0.3 18/05/15 JavaScript eLearn.js - by Arne Westphal
+* v1.0.4 18/06/07 JavaScript eLearn.js - by Arne Westphal
 * eLearning Buero MIN-Fakultaet - Universitaet Hamburg
 * touch-script base by PADILICIOUS.COM and MACOSXAUTOMATION.COM
 * uses ResizeSensor by Marc J. Schmidt. https://github.com/marcj/css-element-queries/
@@ -8,8 +8,8 @@
 // For more intuitive usage of functions. (e.g. eLearnJS.showNext())
 var eLearnJS = eLearnJS || {};
 
-eLearnJS.VERSION_NR = "1.0.3";
-eLearnJS.VERSION_DATE = "05/2018";
+eLearnJS.VERSION_NR = "1.0.4";
+eLearnJS.VERSION_DATE = "06/2018";
 
 eLearnJS.actions = {
     CONTENT_RESIZE : "ContentResize",
