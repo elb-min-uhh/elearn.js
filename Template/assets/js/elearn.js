d94bd2bab90037448abd1c2ad1dc506841230aaf
Arne Westphal <westphal.arne@web.de>
2018-06-07 12:48:40 +0200

    fix/hasScrollbar: fixed missing semicolons

--- Template/assets/js/elearn.js
+++ Template/assets/js/elearn.js
@@ -3047,33 +3047,33 @@ eLearnJS.QueryString = function () {
 
 eLearnJS.hasScrollbar = function() {
     // The Modern solution
-  if (typeof window.innerWidth === 'number')
-    return window.innerWidth > document.documentElement.clientWidth
+    if (typeof window.innerWidth === 'number')
+        return window.innerWidth > document.documentElement.clientWidth;
 
-  // rootElem for quirksmode
-  var rootElem = document.documentElement || document.body
+    // rootElem for quirksmode
+    var rootElem = document.documentElement || document.body;
 
-  // Check overflow style property on body for fauxscrollbars
-  var overflowStyle
+    // Check overflow style property on body for fauxscrollbars
+    var overflowStyle;
 
-  if (typeof rootElem.currentStyle !== 'undefined')
-    overflowStyle = rootElem.currentStyle.overflow
+    if (typeof rootElem.currentStyle !== 'undefined')
+        overflowStyle = rootElem.currentStyle.overflow;
 
-  overflowStyle = overflowStyle || window.getComputedStyle(rootElem, '').overflow
+    overflowStyle = overflowStyle || window.getComputedStyle(rootElem, '').overflow;
 
     // Also need to check the Y axis overflow
-  var overflowYStyle
+    var overflowYStyle;
 
-  if (typeof rootElem.currentStyle !== 'undefined')
-    overflowYStyle = rootElem.currentStyle.overflowY
+    if (typeof rootElem.currentStyle !== 'undefined')
+        overflowYStyle = rootElem.currentStyle.overflowY;
 
-  overflowYStyle = overflowYStyle || window.getComputedStyle(rootElem, '').overflowY
+    overflowYStyle = overflowYStyle || window.getComputedStyle(rootElem, '').overflowY;
 
-  var contentOverflows = rootElem.scrollHeight > rootElem.clientHeight
-  var overflowShown    = /^(visible|auto)$/.test(overflowStyle) || /^(visible|auto)$/.test(overflowYStyle)
-  var alwaysShowScroll = overflowStyle === 'scroll' || overflowYStyle === 'scroll'
+    var contentOverflows = rootElem.scrollHeight > rootElem.clientHeight;
+    var overflowShown = /^(visible|auto)$/.test(overflowStyle) || /^(visible|auto)$/.test(overflowYStyle);
+    var alwaysShowScroll = overflowStyle === 'scroll' || overflowYStyle === 'scroll';
 
-  return (contentOverflows && overflowShown) || (alwaysShowScroll)
+    return (contentOverflows && overflowShown) || (alwaysShowScroll);
 };
 
 eLearnJS.scrollbarBefore = false;
