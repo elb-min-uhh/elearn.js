7f97baecf7be543c52c2ad606a6b551c846a09cb
guerratron <dinertron@gmail.com>
2022-02-01 02:09:53 +0100

    Create elearnjs-es.js
    Translation file for "es" (Spanish)

---
+++ Template/assets/lang/elearnjs-es.js
@@ -0,0 +1,26 @@
+/**
+ * elearn.js localization file for "es". (Spanish)
+ * elearn.js archivo traducción para "es" (Español)
+ * by GuerraTron22 <dinertron@gmail.com> - Juanjo Guerra Haba
+ */
+eLearnJS.localization.es = {
+    "backbutton": "Atrás",
+    "backbutton.title": "Atrás",
+    "togglesections": "Ver",
+    "togglesections.title": "Mostrar/ocultar secciones",
+    "title": "Resúmen",
+    "title.title": "Resúmen del contenido",
+    "menu.title": "Menú",
+    "help": "Ayuda",
+    "help.title": "Mostrar la ayuda",
+    "menu.print": "Imprimir",
+    "menu.share": "Compartir",
+    "menu.imprint": "Imprint",
+    "menu.source": "Descargar Fuente",
+    "menu.pdf": "Descargar PDF",
+    "menu.epub": "Descargar EPUB",
+    "imprint.uses": 'Usa el script de eLearn.js v' + eLearnJS.VERSION_NR + ' | ' + eLearnJS.VERSION_DATE + ' | '
+        + '<a href="https://www.min.uni-hamburg.de/en/imprint.html">Imprint</a>',
+    "imprint.footer": "El autor de esta publicación es responsable del contenido.",
+    "tooltipnext": "Siguiente",
+};
